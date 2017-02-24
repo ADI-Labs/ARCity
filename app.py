d7d6d2b27587b917cf53366faaf1ac57ddb7026e
Garrison <garrison.grogan@columbia.edu>
2017-02-24 16:31:15 -0500

    changed local host settings

--- app.py
+++ app.py
@@ -2,14 +2,15 @@ from flask import Flask, json, render_template
 
 app = Flask(__name__)
 API_KEYS_F = "config/keys.json"
+
 with open(API_KEYS_F, "r") as keys_f:
     API_KEYS = json.load(keys_f)
 
-
+mkey = API_KEYS["maps_embed_api"]
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html",maps_embed_api=API_KEYS["maps_embed_api"])
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,host="0.0.0.0")
