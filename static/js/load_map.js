2251f80c1b9f661dd7b8cf08001a0bd1be2cd239
Garrison Grogan <garrison.grogan@columbia.edu>
2017-03-03 15:12:22 -0500

    added basic google search box

--- static/js/load_map.js
+++ static/js/load_map.js
@@ -22,4 +22,10 @@ function initialize() {
             }
         }
     );
+
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
 }
