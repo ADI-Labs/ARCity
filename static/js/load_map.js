32613125d6c16c0ec33627cd3554ed1ab99bdf6b
Garrison Grogan <garrison.grogan@columbia.edu>
2017-03-24 15:53:31 -0400

    markers now are created, searchbox listener works, streetview does not show markers

--- static/js/load_map.js
+++ static/js/load_map.js
@@ -6,8 +6,8 @@ function initialize() {
         lng: -71.098326
     };
     const map = new google.maps.Map(document.getElementById('map'), {
-      center: fenway,
-      zoom: 14
+        center: fenway,
+        zoom: 14
     });
 
     const panorama = new google.maps.StreetViewPanorama(
@@ -27,20 +27,65 @@ function initialize() {
     // Create the search box and link it to the UI element.
     const input = document.getElementById('pac-input');
     const searchBox = new google.maps.places.SearchBox(input);
+
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+        searchBox.setBounds(map.getBounds());
+    });
+
     var markers = [];
     // Listen for the event fired when the user selects a prediction and retrieve
     // more details for that place.
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
     searchBox.addListener('places_changed', function() {
-      var service = new google.maps.places.PlacesService(panorama);
-             service.nearbySearch({
-               location: panorama.getPocation(),
-               radius: 500,
-               type: ['store']
-             }, callback);
-    })
+        console.log("what the fuck");
+        console.log(searchBox.getPlaces())
+        const places = searchBox.getPlaces();
 
+        if (places.length == 0) {
+            return;
+        }
+        console.log("hello");
+        // Clear out the old markers.
+        markers.forEach(function(marker) {
+            marker.setMap(null);
+        });
+        markers = [];
+        console.log("what");
+        // For each place, get the icon, name and location.
+        const bounds = new google.maps.LatLngBounds();
+        places.forEach(function(place) {
+            if (!place.geometry) {
+                console.log("Returned place contains no geometry");
+                return;
+            }
+            const icon = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
+            };
 
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+                map: map,
+                icon: icon,
+                title: place.name,
+                position: place.geometry.location
+            }));
 
-  panorama.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+            if (place.geometry.viewport) {
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
+        });
+        console.log(markers);
+        map.fitBounds(bounds);
+    });
+    panorama.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
 }
