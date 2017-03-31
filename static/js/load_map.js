6b10c92f10accd29e2c77c8d49b448a6a4e5d9ac
Alex Yu <afy2103@columbia.edu>
2017-03-31 17:31:17 -0400

    Linked panorama and map views

--- static/js/load_map.js
+++ static/js/load_map.js
@@ -9,21 +9,19 @@ function initialize() {
         center: fenway,
         zoom: 14
     });
-
-    const panorama = new google.maps.StreetViewPanorama(
-        document.getElementById('pano'), {
-            position: map.center,
-            pov: {
-                heading: 34,
-                pitch: 10
-            },
-            motionTracking: true,
-            motionTrackingControlOptions: {
-                position: google.maps.ControlPosition.LEFT_BOTTOM
-            }
+    const panorama = map.getStreetView();
+    panorama.setOptions({
+        position: map.center,
+        pov: {
+            heading: 180,
+            pitch: 0
+        },
+        visible: true,
+        motionTracking: true,
+        motionTrackingControlOptions: {
+            position: google.maps.ControlPosition.LEFT_BOTTOM
         }
-    );
-    map.setStreetView(panorama);
+    });
 
     // Create the search box and link it to the UI element.
     const input = document.getElementById('pac-input');
@@ -61,12 +59,12 @@ function initialize() {
                 position: place.geometry.location
             });
             const infowindow = new google.maps.InfoWindow({
-              content: '<span style="padding: 0px; text-align:left" align="left"><h5>' + place.name + '&nbsp; &nbsp; ' + place.rating
-                            + '</h5><p>' + place.formatted_address + '<br />' + place.formatted_phone_number + '<br />' +
-                            '<a  target="_blank" href=' + place.website + '>' + place.website + '</a></p>'
+                content: '<span style="padding: 0px; text-align:left" align="left"><h5>' + place.name + '&nbsp; &nbsp; ' + 
+                    place.rating + '</h5><p>' + place.formatted_address + '<br />' + place.formatted_phone_number + '<br />' +
+                    '<a  target="_blank" href=' + place.website + '>' + place.website + '</a></p>'
             });
             nextMarker.addListener('click',() => {
-              infowindow.open(panorama, nextMarker)
+                infowindow.open(panorama, nextMarker);
             });
             markers.push(nextMarker);
         });
