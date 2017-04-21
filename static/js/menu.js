3e1a689c4abe6d9f1998e9234a37fa0cb7652442
Alex Yu <afy2103@columbia.edu>
2017-04-21 16:31:47 -0400

    Add close function to menu buttons

--- static/js/menu.js
+++ static/js/menu.js
@@ -2,14 +2,20 @@ $(document).ready(() => {
     const open = $('.open-nav');
     const close = $('.close');
     const overlay = $('.overlay');
+    const stores = $('#Stores');
+    const restaurants = $('#Restaurants');
+
+    function closeMenu() {
+        overlay.hide();
+        $('#wrapper').removeClass('toggled');
+    }
 
     open.click(() => {
         overlay.show();
         $('#wrapper').addClass('toggled');
     });
 
-    close.click(() => {
-        overlay.hide();
-        $('#wrapper').removeClass('toggled');
-    });
+    close.click(closeMenu);
+    stores.click(closeMenu);
+    restaurants.click(closeMenu);
 });
